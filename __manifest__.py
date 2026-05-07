b426952b06fced741ede0621d5fe4bc89a54c6c8
rafael <rafael@gmail.com>
2026-05-07 09:39:24 +0100

    primer commit, definición manifest

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name":"Gestion de Tareas",
+    "version":"1.0",
+    "summary":"Gestión básica de tareas asociadas a clientes",
+    "category":"Productivity",
+    "author":"Rafael Martín PPF1889",
+    "depends":["base","sale","contacts"],
+    "data":[
+        "security/ir.model.access.csv",
+        "views/tarea_views.xml",
+    ],
+    "installable":True,
+    "application":True,
+}
